72ef63532e213702c70d2aca25198e71d7c06390
PL LAB <PL LAB@PLLAB26>
2023-08-10 12:22:08 +0530

    first commit

--- src/mycalculator/mysourcecode.java
+++ src/mycalculator/mysourcecode.java
@@ -1,5 +1,14 @@
 package mycalculator;
-
 public class mysourcecode {
+	
+	public static void main(String[] args)
+	{
+		String hungry = "yes";
+		if (hungry == "yes")
+			System.out.println("eat something");
+		else {
+			System.out.println("Do work");
+		}
+	}
 
 }
